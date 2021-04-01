cbeb261fef8c328633e5be72bcaa40054169d044
Arne Köhn <arne@chark.eu>
2021-04-01 10:46:12 +0200

    trained_weights: update architect

--- 2021-trained-weights.sh
+++ 2021-trained-weights.sh
@@ -28,7 +28,7 @@ if [[ ! -f .setup_complete ]]; then
     setup_spigot_plugin f5f6e564739031be453d4d9b3e90eb64bef4e403
     # setup_spigot_woz_plugin
     setup_infrastructure 42894960f7080bd8ad0d4de7582d422c0a6934b4
-    setup_simple-architect 5e0fbb35a6642e1fbc502b66842604470aaaa2ba
+    setup_simple-architect 162e526c801212b4fb97ecfccef892bae2ebad8c
     cp ../configs/broker-config-2021-trained-weights.yaml infrastructure/broker/broker-config.yaml
     if [[ $(hostname) = "minecraft" ]]; then
 	# We use an external questionnaire for these experiments
