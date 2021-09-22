7f0231920797401ea9571f9a8c12ed56cbbf027d
wichlacz <wichlacz@cs.uni-saarland.de>
2021-09-22 12:24:11 +0200

    new learned plans and weights

---
+++ configs/2021-one-instruction-phase-2/third-learned/weights-time.lisp
@@ -0,0 +1,30 @@
+( 
+(!build-floor 6.0 66.0 6.0 5.0 3.0 4.0 ) 11156.682697448863 
+(!place-block-hidden stone 7.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 8.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 9.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 10.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 6.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 7.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 8.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 9.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 10.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 6.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 7.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 8.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 9.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block stone 6.0 67.0 6.0 100.0 100.0 100.0 ) 5965.221692336564 
+(!place-block stone 10.0 67.0 6.0 6.0 67.0 6.0 ) 11814.39443428483 
+(!place-block stone 10.0 68.0 6.0 10.0 67.0 6.0 ) 4400.067704826183 
+(!place-block stone 9.0 68.0 6.0 10.0 68.0 6.0 ) 5094.863641262804 
+(!place-block stone 8.0 68.0 6.0 9.0 68.0 6.0 ) 4646.382533700897 
+(!place-block stone 7.0 68.0 6.0 8.0 68.0 6.0 ) 4646.382533700897 
+(!place-block stone 6.0 68.0 6.0 7.0 68.0 6.0 ) -1449.1050371220838 
+(!place-block stone 10.0 67.0 8.0 6.0 68.0 6.0 ) 4233.973595768338 
+(!place-block stone 6.0 67.0 8.0 10.0 67.0 8.0 ) 7918.202327687202 
+(!place-block stone 6.0 68.0 8.0 6.0 67.0 8.0 ) 6344.651140541956 
+(!place-block stone 7.0 68.0 8.0 6.0 68.0 8.0 ) 14720.806609886578 
+(!place-block stone 8.0 68.0 8.0 7.0 68.0 8.0 ) 1441.8817351511386 
+(!place-block stone 9.0 68.0 8.0 8.0 68.0 8.0 ) 1441.8817351511386 
+(!place-block stone 10.0 68.0 8.0 9.0 68.0 8.0 ) -3180.353133690309 
+ ) 
--- configs/2021-one-instruction-phase-2/weights-deletionsascosts-time.lisp
+++ configs/2021-one-instruction-phase-2/weights-deletionsascosts-time.lisp
@@ -1,30 +1,34 @@
 ( 
-(!build-floor 6.0 66.0 6.0 5.0 3.0 4.0 ) 11156.682697448863 
-(!place-block-hidden stone 7.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 8.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 9.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 10.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 6.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 7.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 8.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 9.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 10.0 66.0 7.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 6.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 7.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 8.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
-(!place-block-hidden stone 9.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
-(!place-block stone 6.0 67.0 6.0 100.0 100.0 100.0 ) 5965.221692336564 
-(!place-block stone 10.0 67.0 6.0 6.0 67.0 6.0 ) 11814.39443428483 
-(!place-block stone 10.0 68.0 6.0 10.0 67.0 6.0 ) 4400.067704826183 
-(!place-block stone 9.0 68.0 6.0 10.0 68.0 6.0 ) 5094.863641262804 
-(!place-block stone 8.0 68.0 6.0 9.0 68.0 6.0 ) 4646.382533700897 
-(!place-block stone 7.0 68.0 6.0 8.0 68.0 6.0 ) 4646.382533700897 
-(!place-block stone 6.0 68.0 6.0 7.0 68.0 6.0 ) -1449.1050371220838 
-(!place-block stone 10.0 67.0 8.0 6.0 68.0 6.0 ) 4233.973595768338 
-(!place-block stone 6.0 67.0 8.0 10.0 67.0 8.0 ) 7918.202327687202 
-(!place-block stone 6.0 68.0 8.0 6.0 67.0 8.0 ) 6344.651140541956 
-(!place-block stone 7.0 68.0 8.0 6.0 68.0 8.0 ) 14720.806609886578 
-(!place-block stone 8.0 68.0 8.0 7.0 68.0 8.0 ) 1441.8817351511386 
-(!place-block stone 9.0 68.0 8.0 8.0 68.0 8.0 ) 1441.8817351511386 
-(!place-block stone 10.0 68.0 8.0 9.0 68.0 8.0 ) -3180.353133690309 
+(!build-floor-starting 6.0 66.0 6.0 5.0 3.0 4.0 ) 1.0E-6 
+(!place-block stone 7.0 66.0 6.0 100.0 100.0 100.0 ) -1.5734127933902502 
+(!place-block stone 8.0 66.0 6.0 7.0 66.0 6.0 ) 0.22293191891893294 
+(!place-block stone 9.0 66.0 6.0 8.0 66.0 6.0 ) 0.22293191891893294 
+(!place-block stone 10.0 66.0 6.0 9.0 66.0 6.0 ) 0.22293191891893294 
+(!place-block stone 10.0 66.0 7.0 10.0 66.0 6.0 ) -2.0701503371408867 
+(!place-block stone 9.0 66.0 7.0 10.0 66.0 7.0 ) 0.6768337309729342 
+(!place-block stone 8.0 66.0 7.0 9.0 66.0 7.0 ) 0.4179458597019269 
+(!place-block stone 7.0 66.0 7.0 8.0 66.0 7.0 ) 0.4179458597019269 
+(!place-block stone 6.0 66.0 7.0 7.0 66.0 7.0 ) -0.836269950562548 
+(!place-block stone 9.0 66.0 8.0 6.0 66.0 7.0 ) -1.5117531309227636 
+(!place-block stone 8.0 66.0 8.0 9.0 66.0 8.0 ) 0.4179458597019269 
+(!place-block stone 7.0 66.0 8.0 8.0 66.0 8.0 ) 0.4179458597019269 
+(!place-block stone 6.0 66.0 8.0 7.0 66.0 8.0 ) 0.397502965224488 
+(!build-floor-finished 6.0 66.0 6.0 5.0 3.0 4.0 ) 1.0E-6 
+(!build-railing-starting 6.0 67.0 6.0 5.0 1.0 ) 1.0E-6 
+(!place-block stone 6.0 67.0 6.0 6.0 66.0 8.0 ) 0.8319282603684302 
+(!place-block stone 10.0 67.0 6.0 6.0 67.0 6.0 ) -0.32368322846686437 
+(!place-block stone 10.0 68.0 6.0 10.0 67.0 6.0 ) -0.7336089824481339 
+(!place-block stone 9.0 68.0 6.0 10.0 68.0 6.0 ) 0.6768337309729342 
+(!place-block stone 8.0 68.0 6.0 9.0 68.0 6.0 ) 0.4179458597019269 
+(!place-block stone 7.0 68.0 6.0 8.0 68.0 6.0 ) 0.4179458597019269 
+(!place-block stone 6.0 68.0 6.0 7.0 68.0 6.0 ) 0.4179458597019269 
+(!build-railing-finished 6.0 67.0 6.0 5.0 1.0 ) 1.0E-6 
+(!build-railing 10.0 67.0 8.0 5.0 2.0 ) 0.35649981847040996 
+(!place-block-hidden stone 6.0 67.0 8.0 6.0 68.0 6.0 ) 0.0 
+(!place-block-hidden stone 10.0 67.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 10.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 9.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 8.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 7.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
+(!place-block-hidden stone 6.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
  ) 
