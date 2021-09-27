c1c2d7179b948a2c092224ea96a66d93ae7a40d4
wichlacz <wichlacz@cs.uni-saarland.de>
2021-09-27 14:57:26 +0200

    new weights and plans

--- configs/2021-one-instruction-phase-2/fourth-learned/weights-time.lisp
+++ configs/2021-one-instruction-phase-2/fourth-learned/weights-time.lisp
@@ -1,5 +1,5 @@
 ( 
-(!build-floor 6.0 66.0 6.0 5.0 3.0 4.0 ) 11365.10815643573 
+(!build-floor 6.0 66.0 6.0 5.0 3.0 4.0 ) 10654.552839565118 
 (!place-block-hidden stone 7.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
 (!place-block-hidden stone 8.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
 (!place-block-hidden stone 9.0 66.0 6.0 100.0 100.0 100.0 ) 0.0 
@@ -13,16 +13,16 @@
 (!place-block-hidden stone 7.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
 (!place-block-hidden stone 8.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
 (!place-block-hidden stone 9.0 66.0 8.0 100.0 100.0 100.0 ) 0.0 
-(!build-railing-starting 6.0 67.0 6.0 5.0 1.0 ) 1.0E-6 
-(!place-block stone 6.0 67.0 6.0 100.0 100.0 100.0 ) 6107.202320738137 
-(!place-block stone 10.0 67.0 6.0 6.0 67.0 6.0 ) 11242.498282667611 
-(!place-block stone 10.0 68.0 6.0 10.0 67.0 6.0 ) 4158.182068524025 
-(!place-block stone 9.0 68.0 6.0 10.0 68.0 6.0 ) 5110.510168218946 
-(!place-block stone 8.0 68.0 6.0 9.0 68.0 6.0 ) 4570.811740282787 
-(!place-block stone 7.0 68.0 6.0 8.0 68.0 6.0 ) 4570.811740282787 
-(!place-block stone 6.0 68.0 6.0 7.0 68.0 6.0 ) -977.1138934054493 
-(!build-railing-finished 6.0 67.0 6.0 5.0 1.0 ) 1.0E-6 
-(!build-railing 10.0 67.0 8.0 5.0 2.0 ) -5794.916659086261 
+(!build-railing-starting 6.0 67.0 6.0 5.0 1.0 ) 1.0 
+(!place-block stone 6.0 67.0 6.0 100.0 100.0 100.0 ) 5989.7510640395585 
+(!place-block stone 10.0 67.0 6.0 6.0 67.0 6.0 ) 11627.046430727527 
+(!place-block stone 10.0 68.0 6.0 10.0 67.0 6.0 ) 4342.55615009924 
+(!place-block stone 9.0 68.0 6.0 10.0 68.0 6.0 ) 5115.400640085071 
+(!place-block stone 8.0 68.0 6.0 9.0 68.0 6.0 ) 4673.189339356113 
+(!place-block stone 7.0 68.0 6.0 8.0 68.0 6.0 ) 4673.189339356113 
+(!place-block stone 6.0 68.0 6.0 7.0 68.0 6.0 ) -1294.7392165887304 
+(!build-railing-finished 6.0 67.0 6.0 5.0 1.0 ) 1.0 
+(!build-railing 10.0 67.0 8.0 5.0 2.0 ) 2640.9769884734214 
 (!place-block-hidden stone 6.0 67.0 8.0 6.0 68.0 6.0 ) 0.0 
 (!place-block-hidden stone 10.0 67.0 8.0 100.0 100.0 100.0 ) 0.0 
 (!place-block-hidden stone 10.0 68.0 8.0 100.0 100.0 100.0 ) 0.0 
